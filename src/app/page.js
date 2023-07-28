ef6d0a42397cd6510b9ac67fe2d607061813a5a3
Bablukmr <bablukmr800@gmail.com>
2023-07-28 22:02:39 +0530

    work1

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
+'use client'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const [data,setData]=useState(0)
+  function handleClick(){
+    setData(data+1)
+  }
+  function handledec(){
+    if(data>0){
+      setData(data-1)
+    }
+  }
   return (
     <main className={styles.main}>
-      Bablu kumar
+      <h1>Bablu kumar</h1>
+      {data}
+      <button onClick={handleClick}>Click Me +</button>
+      <button onClick={handledec}>Click Me -</button>
     </main>
   )
 }
+
