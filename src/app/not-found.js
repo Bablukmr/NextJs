35e272b0afc1f62e057df632c20bb8f72959d54e
Bablukmr <bablukmr800@gmail.com>
2023-07-29 23:06:28 +0530

    fetch data and render server side

--- src/app/not-found.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound() {
-  return (
-    <div>
-        <h1>This Page is Not Available in website</h1>
-    </div>
-  )
-}
--- src/app/page.js
+++ src/app/page.js
@@ -10,15 +10,8 @@ export default function Home(){
 const router=useRouter()
   return(
     <div>
-      <h2>This Is Home Page</h2>
-      <h1>Bablu Kumar</h1>
-      <Link href='/login'> Go To LogIn Page</Link><br/>
-      <Link href='/about'> Go To About Page</Link><br/><br/><br/>
-      <button onClick={()=>navigate('/navigation')}>Navigate</button><br/>
-      <button onClick={()=>navigate('/login')}>Login</button><br/>
-      <button onClick={()=>navigate('/about/aboutstudents')}>AboutS Students</button><br/>
-      <button style={{color:"blue"}} onClick={()=>navigate("/studentlist")}>Go To Students List</button>
-
+     <h1>Fetch Product List With API</h1>
+     <Link href='/productlist'>Go To Product Page</Link>
     </div>
   )
 }
