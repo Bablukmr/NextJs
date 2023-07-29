7a258535fe494b440fd2ea3c47f0ba349253c3b5
Bablukmr <bablukmr800@gmail.com>
2023-07-29 20:51:07 +0530

    Middleware

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+
+import { NextResponse } from "next/server";
+
+export function middleware(request){
+    // console.log(middleware)
+    if(request.nextUrl.pathname!=="/login"){
+        return NextResponse.redirect(new URL("/login",request.url))
+    }
+}
