318720e7fc34de8277ff158f21fef18f37cc23d7
Bablukmr <bablukmr800@gmail.com>
2023-07-30 13:31:46 +0530

    Image and font optimization

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
-'use client'
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import {Roboto} from 'next/font/google'
+
+const roboto=Roboto({
+  weight:'100',
+  subsets:['latin'],
+  display:'swap'
+})
 
 export default function Home(){
 
-  function navigate(name){
-    router.push(name)
-  }
-const router=useRouter()
   return(
     <div>
      <h1>Fetch Product List With API</h1>
+     <h1 className={roboto.className}> This Is Google font wit Next.js</h1>
      <Link href='/productlist'>Go To Product Page</Link>
     </div>
   )
--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,34 +1,25 @@
-// "use client";
-// import { useEffect, useState } from "react";
-
+import Image from "next/image";
 import Product from "./product";
+import bk from '../../../public/bk.jpg'
 
 async function datalist(){
     const res = await fetch("https://dummyjson.com/products");
     const datas = await res.json();
-    // console.log(datas);
     return datas.products
   }
 export default async function Page() {
-//   const [data, setdata] = useState([]);
  let products= await datalist()
-// console.log(products)
-//   async function fetchData() {
-//     const res = await fetch("https://dummyjson.com/products");
-//     const datas = await res.json();
-//     console.log(datas);
-//     setdata(datas.products);
-//   }
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
+
   return (
     <>
       <h1>Product List</h1>
+      <Image  src={bk} width='200' height='200'/>
       {products.map((item) =>(  
         <div key={item.id}>
             <h4>{item.title}, Price: $ {item.price}</h4>
-            <img src={item.images[0]}/>
+            {/* <img src={item.images[0]} width='200' height='200'/> */}
+      <Image   src={item.images[0]} width='200' height='200'/>
+
             <Product title={item.title}/>
         </div>
 
@@ -37,4 +28,10 @@ export default async function Page() {
       )}
     </>
   );
+}
+export function generateMetadata(){
+  return{
+    title:"Mobile List",
+    description:"Mobile description"
+  }
 }
